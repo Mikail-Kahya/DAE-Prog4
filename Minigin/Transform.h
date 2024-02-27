f9d50011d7014132c45d2d7f1c434ccb8c16f364
Mikail <mikailkahya@gmail.com>
2024-02-27 17:21:25 +0100

    Feat: Transform with functions

--- Minigin/Transform.h
+++ Minigin/Transform.h
@@ -9,11 +9,19 @@ namespace mk
 		Transform();
 
 		const glm::vec3& GetPosition() const { return m_Position; }
+		const glm::vec3& GetRotation() const { return m_Rotation; }
+		const glm::vec3& GetScale() const { return m_Scale; }
 
-		void AddWorldOffset(float x = 0.f, float y = 0.f, float z = 0.f);
+		void AddOffset(float x = 0.f, float y = 0.f, float z = 0.f);
+		void AddRotation(float x = 0.f, float y = 0.f, float z = 0.f);
+		void AddScale(float x = 0.f, float y = 0.f, float z = 0.f);
 
+		void SetPosition(const glm::vec3& position);
 		void SetPosition(float x, float y, float z);
-
+		void SetRotation(const glm::vec3& rotation);
+		void SetRotation(float x, float y, float z);
+		void SetScale(const glm::vec3& scale);
+		void SetScale(float x, float y, float z);
 
 	private:
 		glm::vec3 m_Position{};
