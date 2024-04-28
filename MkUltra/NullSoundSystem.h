e6208488de8017ebc2fdcadf2ec6b043e85f9249
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-04-28 18:03:01 +0200

    feat: null sound system

---
+++ MkUltra/NullSoundSystem.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "SoundSystem.h"
+
+namespace mk
+{
+	class NullSoundSystem : public SoundSystem
+	{
+	public:
+		NullSoundSystem()			= default;
+		~NullSoundSystem() override = default;
+
+		NullSoundSystem(const NullSoundSystem& other)					= delete;
+		NullSoundSystem(NullSoundSystem&& other) noexcept				= delete;
+		NullSoundSystem& operator=(const NullSoundSystem& other)		= delete;
+		NullSoundSystem& operator=(NullSoundSystem&& other)	noexcept	= delete;
+
+		void Play(sound_id, float) override {}
+		void Pause(SoundHandle&) override {}
+		void Unpause(SoundHandle&) override {}
+		void Stop(SoundHandle&) override {}
+		void PlayAudio(sound_id, SoundHandle&) override {}
+		void StopAll() override {}
+	};
+}
